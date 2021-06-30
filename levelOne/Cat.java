cce8f126baa857e9a9cd9a8b94eaa8d14836d992
DESKTOP-H9D4I39\Hi <gkdsldk@naver.com>
2021-06-30 23:12:41 +0900

    Cat 파일 생성
    Baekjoon levelOne Cat
    First Commit

---
+++ levelOne/Cat.java
@@ -0,0 +1,17 @@
+package levelOne;
+
+/*
+ * 아래 형태를 출력한다
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+public class Cat {
+	public static void main(String[] args) {
+		System.out.println("\\" + " " +  " " +  " " +  " " + "/\\");
+		System.out.println(" " + ")"+ " " +  " " + "(" + " " +  "'" + ")");
+		System.out.println("(" + " " + " " + "/" + " " + " "  + ")");
+		System.out.println(" " + "\\"+"(__)|");
+	}
+}
