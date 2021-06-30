17e417c8fa0cf18291769fe97e3bf8c54d3941ea
DESKTOP-H9D4I39\Hi <gkdsldk@naver.com>
2021-06-30 23:09:59 +0900

    Krill 파일 생성
    Baekjoon levelOne Krill
    First Commit

---
+++ levelOne/Kriii.java
@@ -0,0 +1,9 @@
+package levelOne;
+
+// 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한번씩 출력한다
+public class Kriii {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
