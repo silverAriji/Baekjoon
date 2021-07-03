c5a293ecb8a992e1a5d66df05eee2e026d5ee20b
DESKTOP-H9D4I39\Hi <gkdsldk@naver.com>
2021-07-03 17:02:13 +0900

    AmultipleB 파일 생성
    Baekjoon levelOne AmultipleB
    First Commit

---
+++ levelOne/AmultipleB.java
@@ -0,0 +1,24 @@
+package levelOne;
+
+import java.util.Scanner;
+
+/*
+문제 : 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오. 
+입력 : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+출력 : 첫째 줄에 A×B를 출력한다.
+*/
+public class AmultipleB {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a*b);
+	}
+
+}
