d9264c826fab15064adbc3b807318691dddc81d3
DESKTOP-H9D4I39\Hi <gkdsldk@naver.com>
2021-07-04 23:11:53 +0900

    ABCremainder 파일 생성
    Baekjoon levelOne ABCremainder
    First Commit

---
+++ levelOne/ABCremainder.java
@@ -0,0 +1,28 @@
+package levelOne;
+
+import java.util.Scanner;
+
+/*
+문제 : 
+1. (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+2. (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+3. 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+*/
+
+public class ABCremainder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+
+	}
+
+}
