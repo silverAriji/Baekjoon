ef7979004929e7d3c5010c38eea12ecc55f78d77
silverAriji <dmjku16@gmail.com>
2021-07-11 08:05:21 +0900

    TestGrade 커밋
    Baekjoon levelTwo TestGrade
    Commit Test

---
+++ levelTwo/TestGrade.java
@@ -0,0 +1,34 @@
+package levelTwo;
+
+import java.util.Scanner;
+
+/*
+문제 : 시험 점수를 입력받아 
+      90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+입력 : 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+출력 : 
+*/
+public class TestGrade {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int score = sc.nextInt();
+		
+		if(score >=0 && score<=100) {
+			if(score >= 90 && score <= 100) {
+				System.out.println("A");
+			}else if(score >=80 && score <90) {
+				System.out.println("B");
+			}else if(score >=70 && score <80) {
+				System.out.println("C");
+			}else if(score >=60 && score<70) {
+				System.out.println("D");
+			}else {
+				System.out.println("F");
+			}	
+		}else {
+			System.out.println("점수를 다시 확인하시기 바랍니다.");
+		}
+	}
+}
